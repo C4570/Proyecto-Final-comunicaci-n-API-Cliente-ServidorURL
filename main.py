0ef9d841eff9dafa0a6b2f387572bcc8f8a3282a
Julián F. Britos <julianfrancisco2010@gmail.com>
2023-06-29 14:57:47 -0300

    move:    baking.json -> bin/baking.json          budget.json -> bin/budget.json          health.json -> bin/health.json          inspiration.json -> bin/inspiration.json          recipes.json -> bin/recipes.json modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return """
+async def read_root(request: Request):
+    html_content =  """
         <html>
             <head>
                 <title>T.P. Final</title>
@@ -15,6 +15,28 @@ async def read_root():
             <body>
                 <h1>Bienvenido a nuestra  página web</h1>
                 <p>Esta es una página web creada con FastAPI.</p>
+                <form action="/name" method="post">
+                    <label for="name">Ingrese su nombre:</label><br>
+                    <input type="text" id="name" name="name"><br><br>
+                    <input type="submit" value="Enviar">
+                </form>
+             </body>
+        </html>
+ """
+    return HTMLResponse(content=html_content, status_code=200)
+
+@app.post("/name", response_class=HTMLResponse)
+async def read_item(request: Request):
+    form_data = await request.form()
+    name = form_data["name"]
+    html_content = f"""
+        <html>
+            <head>
+                <title>T.P. Final</title>
+            </head>
+            <body>
+                <h2>Bienvenido {name} a nuestra  página web</h2>
             </body>
         </html>
     """
+    return HTMLResponse(content=html_content, status_code=200)
